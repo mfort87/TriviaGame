6b2dec1b33a1d06ea501ef56f75114c51c317ae8
Matt Fort <mfort8487@gmail.com>
2018-07-07 08:44:46 -0500

    second save

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+var questions = [{
+    question: "In what country are Red Pandas Found?",
+    answersChoices: ["Nepal", "Belize", "Japan", "Lebanon"],
+    answer: 0
+},{
+    question: "Which large cat enjoys swimming?",
+    answerChoices: ["Cheetah", "Jaguar", "Panther", "Lion"],
+    answer: 1
+},{
+    question: "What is the average weight of a newborn elephant?",
+    answerChoices:["350lbs", "150lbs". "200lbs", "275lbs"],
+    answer: 2
+},{
+    question: "What is a common nickname for the Jaguarundi?",
+    answerChoices:["lanky cat", "cat-dog", "otter-cat", "slimmy jimmy cat"],
+    answer: 2
+},{
+    
+}
